20e24b6db37698275fa2cd286c92a5c4b10f02d3
Pavel_Ferrero <89278757+PavelFerrero@users.noreply.github.com>
2025-03-18 11:50:47 +0500

    Update g_06_songs_list.py

--- 1/g_06_songs_list.py
+++ 1/g_06_songs_list.py
@@ -42,7 +42,7 @@ def main():
     # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
     #   А другие три песни звучат ХХХ минут
     s = violator_songs_dict.get('Sweetest Perfection') + violator_songs_dict.get('Policy of Truth') + violator_songs_dict.get('Blue Dress')
-    print('А другие три песни звучат', round(s,3) ,'минут')
+    print('А другие три песни звучат', round(s,2) ,'минут')
     # TODO здесь ваш код
     return'==='
 print(main())
