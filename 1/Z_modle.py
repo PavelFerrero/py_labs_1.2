4e40238f0b6f95903aa8d59422eaaa0616c5c690
Pavel_Ferrero <89278757+PavelFerrero@users.noreply.github.com>
2025-03-18 10:26:10 +0500

    Add files via upload

---
+++ 1/Z_modle.py
@@ -0,0 +1,24 @@
+from a_00_distance import main as l0
+from b_01_circle import main as l1
+from c_02_operations import main as l2
+from d_03_favorite_movies import main as l3
+from e_04_my_family import main as l4
+from f_05_zoo import main as l5
+from g_06_songs_list import main as l6
+from k_07_secret import main as l7
+from l_08_garden import main as l8
+from o_09_shopping import main as l9
+from p_10_store import main as l10
+
+def main():
+    print(l0)
+    print(l1)
+    print(l2)
+    print(l3)
+    print(l4)
+    print(l5)
+    print(l6)
+    print(l7)
+    print(l8)
+    print(l9)
+    print(l10)
